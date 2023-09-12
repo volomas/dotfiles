533cf6ec980a8c2ca27ecae56a383a42c0e098f1
Volodymyr Masliy <thebestvova@gmail.com>
2023-09-12 21:10:08 +0200

    vim quickscope plugin

--- .config/nvim/lua/vmas/packer.lua
+++ .config/nvim/lua/vmas/packer.lua
@@ -96,4 +96,5 @@ return require('packer').startup(function(use)
       -- or                            , branch = '0.1.x',
       requires = { {'nvim-lua/plenary.nvim'} }
   }
+  use("unblevable/quick-scope")
 end)
