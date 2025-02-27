fd7567c5d6115c7c1298331de9644cce179c8b28
Volodymyr Masliy <thebestvova@gmail.com>
2025-02-27 23:33:53 +0100

    delete some stuff

--- .config/yadm/bootstrap.d/yadm-use-ssh.sh
+++ .config/yadm/bootstrap.d/yadm-use-ssh.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 echo "Updating yadm repo origin URL to use ssh"
 yadm remote set-url origin "git@github.com:volomas/dotfiles.git"
-
-pushd "$HOME"
-echo "Init submodules"
-yadm submodule update --recursive --init
-popd
