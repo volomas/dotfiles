741093bbba2669e041813767446c089474c7de23
Volodymyr Masliy <thebestvova@gmail.com>
2025-07-07 14:13:07 +0200

    change structure

--- .config/yadm/bootstrap.d/yadm-use-ssh.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-echo "Updating yadm repo origin URL to use ssh"
-yadm remote set-url origin "git@github.com:volomas/dotfiles.git"
---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#TODO add more, copy originals if exists, etc
+stow -t $HOME tmux
+stow -t $HOME zsh
